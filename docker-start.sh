92cd72fa64522c83005a06f4862315dbdf31a081
eze210 <eze210@gmail.com>
2017-06-09 01:32:59 -0300

    Adding Docker and a basic server. #1 #2

---
+++ docker-start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo docker stop raspy
+
+result=$(sudo docker ps -a | grep raspy)
+if [ ! -z "$result" ]
+then
+  echo "Docker already running."
+  sudo docker rm raspy
+fi
+
+sudo docker run -it -p 9000:10000 --name raspy -v "$PWD":/raspy raspy bash
---
+++ raspy/server.py
@@ -0,0 +1,38 @@
+import SocketServer
+import socket, fcntl, struct
+
+def _get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+
+
+if __name__ == "__main__":
+    HOST, PORT = _get_ip_address('eth0'), 9999
+
+    # Create the server, binding to localhost on port 9999
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
