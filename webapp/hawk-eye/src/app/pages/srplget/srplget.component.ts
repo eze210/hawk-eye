54ddd7059f98a70a96bb2edfdf0e97d371b43a29
Federico Garcia Brion <fedeagb@gmail.com>
2017-08-09 01:38:33 -0300

    Added google maps to getter page. Loads an example route. Will make it load locations that are returned on getter.

--- webapp/hawk-eye/src/app/pages/srplget/srplget.component.ts
+++ webapp/hawk-eye/src/app/pages/srplget/srplget.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ServerService } from '../../shared/server.service'
 
+declare var google: any;
+
 @Component({
   selector: 'app-srplget',
   templateUrl: './srplget.component.html',
@@ -21,6 +23,45 @@ export class SrplgetComponent implements OnInit {
 		        photo.imageData = 'data:image/png;base64,' + photo[1];
 		    };
 		});
+
+    // Maps
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 7,
+        center: {lat: 41.85, lng: -87.65}
+      });
+    directionsDisplay.setMap(map);
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+
+      var waypts = [];
+      var checkboxArray:any[] = [
+          'winnipeg', 'regina','calgary'
+      ];
+      for (var i = 0; i < checkboxArray.length; i++) {
+        waypts.push({
+          location: checkboxArray[i],
+          stopover: true
+        });
+
+      }
+
+      directionsService.route({
+        origin: {lat: 41.85, lng: -87.65},
+        destination: {lat: 49.3, lng: -123.12},
+        waypoints: waypts,
+        optimizeWaypoints: true,
+        travelMode: 'DRIVING'
+      }, function(response, status) {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    }
   }
 
   getLocations(id) {
